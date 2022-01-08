6658d2db8dcf3ba276285ba67ff6a3a4e97c583d
LeeTyrer <lee.4lifee@gmail.com>
2022-01-08 17:35:00 +1000

    fix: manually created installation requirements in setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ from setuptools import find_namespace_packages
 with open("README.md", mode="r") as _file:
     long_description = _file.read()
 
-with open("requirements.txt", mode="r") as requirements:
-    install_requires = list(requirements.read().splitlines())
-
 setup(
     name='GrandExchange-ToolBox',
     version='0.1',
     packages=find_namespace_packages("toolbox"),
-    install_requires=install_requires,
+    install_requires=[
+        "requests",
+        "pandas",
+        "fuzzywuzzy",
+        "matplotlib",
+    ],
     url='https://github.com/Lee-Tyrer/GrandExchange-ToolBox',
     license='',
     author='Lee',
